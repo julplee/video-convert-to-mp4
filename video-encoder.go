a989bdd1064747142cc6faa2241e97886f3fd5f3
Julien Plée <julplee@gmail.com>
2019-09-15 19:00:38 +0200

    Create video-encoder.go

---
+++ video-encoder.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	var files []string
+
+	root := "./video-to-encode"
+
+	log.Println("Reading the video to encode directory...")
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() || filepath.Ext(path) != ".ts" {
+			if path != root {
+				log.Println("WARNING: The file " + path + " will not be encoded")
+			}
+			return nil
+		}
+
+		log.Println("The file " + path + " will be encoded")
+		files = append(files, path)
+		return nil
+	})
+
+	if err != nil {
+		log.Println("ERROR: ", err)
+	}
+
+	log.Println("Now let's encode each video:")
+	for _, file := range files {
+		encodeVideoToMP4(file)
+	}
+
+}
+
+func encodeVideoToMP4(inputVideoFilepath string) {
+	var filename = filepath.Base(inputVideoFilepath)
+	var extension = filepath.Ext(filename)
+	var name = filename[0 : len(filename)-len(extension)]
+
+	// use ffmpeg to encode video to MP4
+	cmd := exec.Command("ffmpeg.exe", "-i", inputVideoFilepath, "-preset", "slow", "-c:a", "aac", "-codec:v", "libx264", "-profile:v", "main", "video-encoded\\"+name+".mp4")
+
+	var buffer bytes.Buffer
+	cmd.Stdout = &buffer
+
+	log.Println("Encode video " + inputVideoFilepath)
+	err := cmd.Run()
+
+	if err != nil {
+		log.Println("ERROR: ", inputVideoFilepath, err)
+	}
+
+	cmd.Wait()
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
